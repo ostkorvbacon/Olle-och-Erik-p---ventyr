db4a54878861a604bdad8b665d855b0de35960da
ostkorvbacon <55699954+ostkorvbacon@users.noreply.github.com>
2019-12-10 10:01:53 +0100

    Add files via upload

---
+++ Affar3/src/Ko.java
@@ -0,0 +1,17 @@
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class Ko
+{
+	private JList _ko;
+	public Ko() 
+	{
+		_ko = new JList(new DefaultListModel());	
+		
+	}
+
+	
+	
+}
---
+++ Affar3/src/Person.java
@@ -0,0 +1,31 @@
+
+public class Person 
+{	
+	private String _namn;
+	private String _nrvar;
+	
+	public Person(String n,String v)
+	{
+		_namn=n;
+		_nrvar=v;
+				
+	}
+
+	public String get_namn() {
+		return _namn;
+	}
+
+	public void set_namn(String _namn) {
+		this._namn = _namn;
+	}
+
+	public String get_nrvar() {
+		return _nrvar;
+	}
+
+	public void set_nrvar(String _nrvar) {
+		this._nrvar = _nrvar;
+	}
+	
+	
+}
