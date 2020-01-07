f6b431a794ccbdfb4dc037fd9bc4eb49510d4346
eriksven107 <57899290+eriksven107@users.noreply.github.com>
2020-01-07 12:21:50 +0100

    Add files via upload

---
+++ Affar6/src/BButt.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class BButt extends JButton
+{
+	private JList _ko;     //Variabel som anväds för att lagra data som är nödvändig för metod-anrop.
+	
+	public BButt (JList ko,String s ) 
+	{
+		super(s);
+		_ko=ko;
+		this.addActionListener(new RemoveListener());
+	}
+	
+	//Specifik lysnare till knappen
+	private class RemoveListener implements ActionListener 
+	{	//Pre:true
+		//Post:Anropar metod som bejänar personen först i kön när knappen aktiveras.
+		public void actionPerformed(ActionEvent e) 
+		{
+			Ko.betjana(_ko);
+		}
+	}
+}
---
+++ Affar6/src/MoveButt.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class MoveButt extends JButton 
+{
+	private JList _list,_ko;   //Variabler som anväds för att lagra data som är nödvändig för metod-anrop.
+	
+	public MoveButt(JList aList,String s,JList ko ) 
+	{
+		super(s);
+		_list = aList;
+		_ko=ko;
+		this.addActionListener(new RemoveListener());
+	}
+	//Specifik lysnare till knappen
+	private class RemoveListener implements ActionListener 
+	{	//Pre:true
+		//Post:Anropar metod som flyttar mellan listor när knappen aktiveras.
+		public void actionPerformed(ActionEvent e) 
+		{
+		
+			Ko.flytta(_list,_ko);
+		}
+	}
+}
